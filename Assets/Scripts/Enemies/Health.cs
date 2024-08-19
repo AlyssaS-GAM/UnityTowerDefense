fc68f03c31f315b79e2f9f7c884540b1dc7a4937
AlyssaS-GAM <alyssa.shulti@icloud.com>
2024-08-18 19:21:01 -0700

    Implement currency

--- Assets/Scripts/Enemies/Health.cs
+++ Assets/Scripts/Enemies/Health.cs
@@ -6,7 +6,7 @@ public class Health : MonoBehaviour
 {
     [Header("Attributes")]
     [SerializeField] private int hitPoints = 2;
-
+    [SerializeField] private int currencyWorth = 50;
     private bool isDestroyed = false;
 
     public void TakeDamage(int dmg)
@@ -16,6 +16,7 @@ public class Health : MonoBehaviour
         if (hitPoints <= 0 && !isDestroyed)
         {
             EnemySpawner.onEnemyDestroy.Invoke();
+            LevelManager.main.IncreaseCurrency(currencyWorth);
             isDestroyed = true;
             Destroy(gameObject);
         }
